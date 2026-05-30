b016e13ce54738ce1cb772a204c3fcafa1735a17
Dax Harris <git@dax.gay>
2026-05-30 09:57:08 -0400

    add dyndns

--- hosts/homeassistant/services/default.nix
+++ hosts/homeassistant/services/default.nix
@@ -2,5 +2,6 @@
 {
     imports = [
         ./homeassistant.nix
+        ./dyndns.nix
     ];
 }
---
+++ hosts/homeassistant/services/dyndns.nix
@@ -0,0 +1,14 @@
+{ config, ... }:
+{
+    sops.secrets.dyndns = {
+        mode = "0444";
+    };
+    services.ddns-updater = {
+        enable = true;
+        environment = {
+            SERVER_ENABLED = "no";
+            CONFIG_FILEPATH = config.sops.secrets.dyndns.path;
+            PERIOD = "5m";
+        };
+    };
+}
