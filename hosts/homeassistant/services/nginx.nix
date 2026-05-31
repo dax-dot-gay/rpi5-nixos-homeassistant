7617690655a1fb52a532379408742ea41e2df74c
Dax Harris <git@dax.gay>
2026-05-30 20:09:21 -0400

    other path

--- hosts/homeassistant/services/nginx.nix
+++ hosts/homeassistant/services/nginx.nix
@@ -16,7 +16,7 @@
             enableACME = true;
             forceSSL = true;
             locations."/" = {
-                proxyPass = "http://127.0.0.1:8123";
+                proxyPass = "http://0.0.0.0:8123";
                 proxyWebsockets = true;
             };
         };
